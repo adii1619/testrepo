8e2f8e79f7f9384714ba5604b83f70b2244081a7
adii1619 <maken8219@gmail.com>
2025-08-02 07:21:09 +0500

    pyhthon file

---
+++ user_input.py
@@ -0,0 +1,26 @@
+first_num= int(input("Enter the first number"))
+
+sec_num=int(input("Enter the second number "))
+
+print(first_num)
+print(sec_num)
+
+result= first_num + sec_num
+
+print(result)
+
+# # # type
+# num2=type(6.8)
+
+# print(num2)
+# num=float(4)
+
+# print(num)
+
+# li=list("HELLO")
+
+# print(li)
+# #type conversion is not permanent
+unicode=u"\U0001f600"
+
+print(unicode)
